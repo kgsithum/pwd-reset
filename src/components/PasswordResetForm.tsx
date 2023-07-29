6ce7f2246dd295e518cadd336cc2b7c3cfc6bda4
sithumamara <sithum.amarasinghe@nzpost.co.nz>
2023-07-30 00:21:39 +1200

    Implement the validation and add types

--- src/components/PasswordResetForm.tsx
+++ src/components/PasswordResetForm.tsx
@@ -3,32 +3,53 @@ import TextInput from './TextInput';
 import Button from './Button';
 import styles from './PasswordResetForm.module.css';
 import Message from './Message';
+import { MessageType } from '../types/MessageType';
+import { InputType } from '../types/InputType';
+import { validatePassword } from '../utils/Validations';
 
 const PasswordResetForm: React.FC = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [isValid, setIsValid] = useState(false);
+  const [message, setMessage] = useState('');
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
-    setIsValid(validatePassword(event.target.value, confirmPassword));
+    setIsValid(validate(event.target.value, confirmPassword));
   };
 
   const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setConfirmPassword(event.target.value);
-    setIsValid(validatePassword(password, event.target.value));
+    setIsValid(validate(password, event.target.value));
   };
 
-  const validatePassword = (password: string, confirmPassword: string) => {
-    // ToDo: Create a separate method to validate.
-    // Validate just the length for now
-    const isPasswordValid = password.length >= 8
+  const validate = (password: string, confirmPassword: string) => {
+    // Validate password
+    const isPasswordValid = validatePassword(password);
+    setDisplayMessage(isPasswordValid, password, confirmPassword);
 
     return isPasswordValid && password === confirmPassword;
   };
 
+  const setDisplayMessage = (isPasswordValid: boolean, password: string, confirmPassword: string): void => {
+    let msg = '';
+    if (isPasswordValid) {
+        if (password === confirmPassword) {
+            msg = 'Password match and meet the requirements!';
+        } else {
+            msg = 'Incorrect confirm new password.';
+        }
+    } else {
+        msg = 'Password does not meet the requirements.';
+    }
+    setMessage(msg);
+  };
+
   const handleReset = () => {
-    alert('Password Reset Successful!');
+    if (isValid) {
+        // Handle backend API request at this point
+        alert('Password Reset Successful!');
+    }
   };
 
   return (
@@ -37,28 +58,35 @@ const PasswordResetForm: React.FC = () => {
             <h1>Reset password</h1>
         </div>
         <div className={styles.textInput}>
+            <label htmlFor="password">Password *</label>
             <TextInput
-                type="password"
+                type={InputType.PASSWORD}
                 value={password}
                 onChange={handlePasswordChange}
                 placeholder="Enter your password"
+                id='password'
             />
         </div>
         <div className={styles.textInput}>
+            <label htmlFor="confirmPassword">Confirm Password *</label>
             <TextInput
-                type="password"
+                type={InputType.PASSWORD}
                 value={confirmPassword}
                 onChange={handleConfirmPasswordChange}
                 placeholder="Confirm your password"
+                id="confirmPassword"
             />
         </div>
         <div className={styles.button}>
             <Button disabled={!isValid} onClick={handleReset} buttonText='Reset' />
         </div>
+        {message &&
         <div className={styles.messageContainer}>
-            {isValid ? 
-                <Message type='success' text='Passwords match and meet the requirements!' /> 
-                : <Message type='success' text='Passwords do not match or do not meet the requirements.' />}
+            <Message type={isValid ? MessageType.SUCCESS : MessageType.ERROR} text={message} /> 
+        </div>
+        }
+        <div className={styles.hintText}>
+            <p>Hint: At least 8 characters, including one number and two special characters, or be greater than 15 characters.</p>
         </div>
     </div>
   );
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import styles from './TextInput.module.css';
+import { InputType } from '../types/InputType';
 
 interface TextInputProps {
-  type: 'text' | 'password';
+  type: InputType;
   value: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
+  id: string;
 }
 
-const TextInput: React.FC<TextInputProps> = ({ type, value, onChange, placeholder }) => {
+const TextInput: React.FC<TextInputProps> = ({ type, value, onChange, placeholder, id }) => {
   return (
     <input className={styles.customInput}
       type={type}
       value={value}
       onChange={onChange}
       placeholder={placeholder}
+      id={id}
     />
   );
 };
